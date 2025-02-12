25ab13b530d14bf333c907d7533f94e17cbc5a82
dallycita <ram241035@uvg.edu.gt>
2025-02-11 21:19:04 -0600

    Update Mains.java

--- Mains.java
+++ Mains.java
@@ -2,11 +2,11 @@
 
 public class Mains {
     public static void main(String[] args) {
-        int size = 3000;
-        int[] numbers = FileHandler.generateRandomNumbers(size);
-        FileHandler.saveToFile(numbers, "numbers.txt");
+        int size = 3000;// Cantidad de números a generar
+        int[] numbers = FileHandler.generateRandomNumbers(size); // Genera números aleatorios
+        FileHandler.saveToFile(numbers, "numbers.txt"); // Guarda en archivo
         
-        int[] data = FileHandler.loadFromFile("numbers.txt");
+        int[] data = FileHandler.loadFromFile("numbers.txt"); // Carga los datos desde el archivo
         
         // Aplicando algoritmos de ordenamiento
         SortRunner.runSorts(data);
