e06e01cfe5cdffdb3067ca9e3f4f6c18da476dc4
dallycita <ram241035@uvg.edu.gt>
2025-02-11 21:28:14 -0600

    Update SortRunner.java

--- SortRunner.java
+++ SortRunner.java
@@ -2,27 +2,27 @@ import java.util.Arrays;
 
 class SortRunner {
     public static void runSorts(int[] data) {
-        int[] copy;
+        int[] copy; // Arreglo para almacenar copias de los datos originales
         
         copy = Arrays.copyOf(data, data.length);
         long start = System.nanoTime();
         Sorts.insertionSort(copy);
         long end = System.nanoTime();
-        System.out.println("Insertion Sort: " + (end - start) / 1e6 + " ms");
+        System.out.println("Insertion Sort: " + (end - start) / 1e6 + " ms"); // Variables para medir el tiempo de ejecución
         
-        copy = Arrays.copyOf(data, data.length);
-        start = System.nanoTime();
-        Sorts.mergeSort(copy);
-        end = System.nanoTime();
-        System.out.println("Merge Sort: " + (end - start) / 1e6 + " ms");
+        copy = Arrays.copyOf(data, data.length); // Crear una copia de los datos
+        start = System.nanoTime(); // Registrar tiempo de inicio
+        Sorts.mergeSort(copy); //  Insertion Sort ejecutar 
+        end = System.nanoTime(); // Registrar tiempo de finalización
+        System.out.println("Merge Sort: " + (end - start) / 1e6 + " ms");  //  Tiempo en milisegundos
         
-        copy = Arrays.copyOf(data, data.length);
+        copy = Arrays.copyOf(data, data.length); // Nueva copia de los datos y con todos los demas pasos de antes
         start = System.nanoTime();
         Sorts.quickSort(copy, 0, copy.length - 1);
         end = System.nanoTime();
         System.out.println("Quick Sort: " + (end - start) / 1e6 + " ms");
         
-        copy = Arrays.copyOf(data, data.length);
+        copy = Arrays.copyOf(data, data.length); // Nueva copia de los datos y con todos los demas pasos de antes
         start = System.nanoTime();
         Sorts.radixSort(copy);
         end = System.nanoTime();
